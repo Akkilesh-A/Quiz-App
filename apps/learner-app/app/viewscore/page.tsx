370718075c911455a9a593499b7005c43f48f958
Akkilesh-A <akkilalagar05@gmail.com>
2024-06-19 02:12:03 +0530

    fixed conditional rendering

--- apps/learner-app/app/viewscore/page.tsx
+++ apps/learner-app/app/viewscore/page.tsx
@@ -1,35 +1,35 @@
 'use client'
 
 import React from 'react'
-import {ScoreIndicator} from "@repo/ui/scoreIndicator"
-import { UseSelector, useSelector } from 'react-redux'
+import { ScoreIndicator } from "@repo/ui/scoreIndicator"
+import { useSelector } from 'react-redux'
 
 const ViewScore = () => {
-
-  const answers = useSelector((state : any) => state.answer)
-  const quiz = useSelector((state : any )=> state.quiz)
+  const answers = useSelector((state: any) => state.answer)
+  const quiz = useSelector((state: any) => state.quiz)
 
   let correct = 0;
-  for(let i=0;i<quiz[0].questions.length;i++){
-    if(answers[0]?.options[i]==quiz[0].questions[i].correctAnswer){
-      correct+=1;
+
+  if (quiz.length > 0 && answers.length > 0 && quiz[0]?.questions) {
+    for (let i = 0; i < quiz[0]?.questions?.length; i++) {
+      if (answers[0]?.options[i] == quiz[0]?.questions[i]?.correctAnswer) {
+        correct += 1;
+      }
     }
   }
 
   return (
     <div>
-        {quiz[0].questions ? 
+      {quiz.length > 0 && answers.length > 0 ? (
         <div>
           <h1 className='text-center font-bold text-[3rem]'>Your Score!</h1>
           <ScoreIndicator value={correct} maxValue={quiz[0].questions.length} />
-        </div> : 
-        <div>Loading </div>
-        }
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   )
 }
-
-
-
 
 export default ViewScore
